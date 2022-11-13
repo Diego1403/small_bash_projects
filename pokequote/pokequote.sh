048ed39da34be4ce011c96bcf1ff2bb87b68e807
Diego Vasquez Revilla <diegovr.1403@gmail.com>
2022-11-13 13:54:33 +0100

    typo

--- pokequote/pokequote.sh
+++ pokequote/pokequote.sh
@@ -4,7 +4,6 @@
 url=https://pokeapi.co/api/v2/pokemon/$(( $RANDOM % 1000 + 1 ))
 
 quote_url=https://motivational-quote-api.herokuapp.com/quotes/random
-#Var= curl --location --request GET $url \ --header 'Content-Type: application/json' \--data-raw '{ "cc_name": "basic", "channel_name": "mychannel"}'
 pokemon=$(curl -s $url |  python3 -c "import sys, json; print(json.load(sys.stdin)['name'])" ) 
 quote=$(curl -s $quote_url |  python3 -c "import sys, json; print(json.load(sys.stdin)['quote'])" )
 
